439977959c592c7dee5b935b2904a4aae3f6543b
jootiee <purrrtov@ya.ru>
2025-11-13 18:27:35 +0300

    fix: moved config to internal, loads from .env

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	BindAddr    string
+	LogLevel    string
+	DatabaseURL string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BindAddr: ":8080",
+		LogLevel: "debug",
+	}
+}
+
+func (c *Config) LoadFromEnv() {
+	if port := os.Getenv("APP_PORT"); port != "" {
+		c.BindAddr = ":" + port
+	}
+	if logLevel := os.Getenv("LOG_LEVEL"); logLevel != "" {
+		c.LogLevel = logLevel
+	}
+
+	if host := os.Getenv("POSTGRES_HOST"); host != "" {
+		user := get("POSTGRES_USER", "postgres")
+		password := get("POSTGRES_PASSWORD", "postgres")
+		dbname := get("POSTGRES_DB", "avito_tech")
+		port := get("POSTGRES_PORT", "5432")
+		c.DatabaseURL = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			host, port, user, password, dbname)
+	}
+}
+
+func get(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
